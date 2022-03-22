0aae10472e50f2aedca14b316f9ec4fb5be07891
กีตาร์ยังไงล่ะ <45527907+dudkinox@users.noreply.github.com>
2022-03-22 22:31:12 +0700

    Update TabNavBar.tsx

--- src/components/TabNavBar.tsx
+++ src/components/TabNavBar.tsx
@@ -2,6 +2,7 @@ import { Tab, Tabs } from "@mui/material";
 import { useState } from "react";
 import HomeIcon from "@mui/icons-material/Home";
 import PersonPinIcon from "@mui/icons-material/PersonPin";
+import { Themes } from "../themes/color";
 
 export default function TabNavBar() {
   const [value, setValue] = useState(0);
@@ -16,8 +17,8 @@ export default function TabNavBar() {
       onChange={handleChange}
       aria-label="icon label tabs example"
     >
-      <Tab icon={<HomeIcon />} label="RECENTS" />
-      <Tab icon={<PersonPinIcon />} label="NEARBY" />
+      <Tab icon={<HomeIcon />} label="HOME" color={Themes.primary} />
+      <Tab icon={<PersonPinIcon />} label="ACCOUNT" color={Themes.primary} />
     </Tabs>
   );
 }
