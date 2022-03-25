5cc9a78690eda8c0aa294f31fdbbaf95d6b4128a
Tossawat <51531950+tanza4432@users.noreply.github.com>
2022-03-25 13:52:18 +0700

    .

--- backend/api/storage.js
+++
@@ -1,8 +0,0 @@
-const admin = require("firebase-admin");
-const config = require("./config");
-const FirebaseApp = admin.initializeApp({
-  credential: admin.credential.cert(config.firebaseConfig),
-  storageBucket: config.firebaseConfig.storageBucket,
-});
-
-module.exports = FirebaseApp.storage();
