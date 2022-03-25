81c9a34e8558f4b96f406c1cfe24c568c1b91eed
กีตาร์อย่างมา <guitar@odds.team>
2022-03-26 02:33:13 +0700

    :guitar:

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,69 @@
-import { AppBar, Container, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Button,
+  Grid,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import "./App.css";
-import TabNavBar from "./components/TabNavBar";
-import { Themes } from "./themes/color";
+import HomeIcon from "@mui/icons-material/Home";
+import PersonIcon from "@mui/icons-material/Person";
+import RestaurantMenuOutlinedIcon from "@mui/icons-material/RestaurantMenuOutlined";
 
 function App() {
   return (
-    <div>
-      <Container maxWidth="xl">
-        <AppBar position="static" sx={{ backgroundColor: Themes.white }}>
-          <Container maxWidth="xl">
-            <Toolbar disableGutters>
-              <Typography
-                variant="h6"
-                noWrap
-                component="div"
-                sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
-              >
-                <img
-                  src="assets/image/logo.png"
-                  width="190"
-                  height="190"
-                  alt=""
-                />
-              </Typography>
-
-              <Typography
-                variant="h6"
-                noWrap
-                component="div"
-                sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
-              >
-                LOGO
-              </Typography>
-            </Toolbar>
-          </Container>
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" style={{ backgroundColor: "#eb1c24" }}>
+          <Toolbar>
+            <Grid container spacing={2}>
+              <Grid item xs={11}>
+                <Typography variant="h6" component="span">
+                  ค้นหาร้านอาหารที่ชอบ เน้นรีวิวจริง อร่อยจริง
+                </Typography>
+              </Grid>
+              <Grid item xs={1}>
+                <PersonIcon />
+              </Grid>
+            </Grid>
+          </Toolbar>
         </AppBar>
-        <TabNavBar />
-      </Container>
-    </div>
+      </Box>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" sx={{ backgroundColor: "white" }}>
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              aria-label="menu"
+            ></IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <img
+                src="assets/image/logo.png"
+                width="120"
+                height="100"
+                alt=""
+              />
+            </Typography>
+            <Button sx={{ color: "#000", fontSize: "26px" }}>
+              <HomeIcon sx={{ fontSize: 50 }} /> &emsp;หน้าแรก
+            </Button>
+            &emsp;
+            <Button sx={{ color: "#000", fontSize: "26px" }}>
+              <RestaurantMenuOutlinedIcon sx={{ fontSize: 50 }} />
+              &emsp; รีวิวร้านอาหาร
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <img
+        src="https://images.pexels.com/photos/2074130/pexels-photo-2074130.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+        style={{ width: "100%", height: "100%" }}
+        alt=""
+      />
+    </>
   );
 }
 
