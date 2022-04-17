3b3500135358e20e9c3ec6940e35fb6d4edb0fac
กีตาร์เอาเอง <guitar@odds.team>
2022-04-17 22:30:00 +0700

    :guitar:

---
+++ src/components/TableAccount.tsx
@@ -0,0 +1,355 @@
+import * as React from "react";
+import { alpha } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import TableSortLabel from "@mui/material/TableSortLabel";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import Checkbox from "@mui/material/Checkbox";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { visuallyHidden } from "@mui/utils";
+import AccountModel from "../models/AccountModel";
+
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+type Order = "asc" | "desc";
+
+function getComparator<Key extends keyof any>(
+  order: Order,
+  orderBy: Key
+): (
+  a: { [key in Key]: number | string },
+  b: { [key in Key]: number | string }
+) => number {
+  return order === "desc"
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort<T>(
+  array: readonly T[] | AccountModel[],
+  comparator: (a: T, b: T) => number
+) {
+  const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) {
+      return order;
+    }
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+interface HeadCellAccount {
+  disablePadding: boolean;
+  id: keyof AccountModel;
+  label: string;
+  numeric: boolean;
+}
+
+const headCells: readonly HeadCellAccount[] = [
+  {
+    id: "email",
+    numeric: false,
+    disablePadding: true,
+    label: "Email",
+  },
+  {
+    id: "name",
+    numeric: false,
+    disablePadding: true,
+    label: "Full name",
+  },
+  {
+    id: "tel",
+    numeric: false,
+    disablePadding: true,
+    label: "Phone",
+  },
+  {
+    id: "type",
+    numeric: false,
+    disablePadding: true,
+    label: "Type",
+  },
+];
+
+interface EnhancedTableProps {
+  numSelected: number;
+  onRequestSort: (
+    event: React.MouseEvent<unknown>,
+    property: keyof AccountModel
+  ) => void;
+  onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  order: Order;
+  orderBy: string;
+  rowCount: number;
+}
+
+function EnhancedTableHead(props: EnhancedTableProps) {
+  const {
+    onSelectAllClick,
+    order,
+    orderBy,
+    numSelected,
+    rowCount,
+    onRequestSort,
+  } = props;
+  const createSortHandler =
+    (property: keyof AccountModel) => (event: React.MouseEvent<unknown>) => {
+      onRequestSort(event, property);
+    };
+
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell padding="checkbox">
+          <Checkbox
+            color="primary"
+            indeterminate={numSelected > 0 && numSelected < rowCount}
+            checked={rowCount > 0 && numSelected === rowCount}
+            onChange={onSelectAllClick}
+            inputProps={{
+              "aria-label": "select all desserts",
+            }}
+          />
+        </TableCell>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? "right" : "left"}
+            padding={headCell.disablePadding ? "none" : "normal"}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : "asc"}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === "desc" ? "sorted descending" : "sorted ascending"}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+interface EnhancedTableToolbarProps {
+  numSelected: number;
+}
+
+const EnhancedTableToolbar = (props: EnhancedTableToolbarProps) => {
+  const { numSelected } = props;
+
+  return (
+    <Toolbar
+      sx={{
+        pl: { sm: 2 },
+        pr: { xs: 1, sm: 1 },
+        ...(numSelected > 0 && {
+          bgcolor: (theme) =>
+            alpha(
+              theme.palette.primary.main,
+              theme.palette.action.activatedOpacity
+            ),
+        }),
+      }}
+    >
+      {numSelected > 0 ? (
+        <Typography
+          sx={{ flex: "1 1 100%" }}
+          color="inherit"
+          variant="subtitle1"
+          component="div"
+        >
+          {numSelected} selected
+        </Typography>
+      ) : (
+        <Typography
+          sx={{ flex: "1 1 100%" }}
+          variant="h6"
+          id="tableTitle"
+          component="div"
+        >
+          Manage Account
+        </Typography>
+      )}
+      {numSelected > 0 ? (
+        <Tooltip title="Delete">
+          <IconButton>
+            <DeleteIcon />
+          </IconButton>
+        </Tooltip>
+      ) : (
+        <></>
+      )}
+    </Toolbar>
+  );
+};
+
+interface TableAccountProps {
+  rows: AccountModel[];
+}
+
+export default function TableAccount({ rows }: TableAccountProps) {
+  const [order, setOrder] = React.useState<Order>("asc");
+  const [orderBy, setOrderBy] = React.useState<keyof AccountModel>("name");
+  const [selected, setSelected] = React.useState<readonly string[]>([]);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const handleRequestSort = (
+    event: React.MouseEvent<unknown>,
+    property: keyof AccountModel
+  ) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
+  const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.checked) {
+      const newSelecteds = rows.map((n) => n.name);
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event: React.MouseEvent<unknown>, name: string) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected: readonly string[] = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1)
+      );
+    }
+
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const isSelected = (name: string) => selected.indexOf(name) !== -1;
+
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Paper sx={{ width: "100%" }}>
+        <EnhancedTableToolbar numSelected={selected.length} />
+        <TableContainer>
+          <Table sx={{ minWidth: 750 }} aria-labelledby="tableTitle">
+            <EnhancedTableHead
+              numSelected={selected.length}
+              order={order}
+              orderBy={orderBy}
+              onSelectAllClick={handleSelectAllClick}
+              onRequestSort={handleRequestSort}
+              rowCount={rows.length}
+            />
+            <TableBody>
+              {stableSort(rows, getComparator(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row, index) => {
+                  const isItemSelected = isSelected(row.name.toString());
+                  const labelId = `enhanced-table-checkbox-${index}`;
+
+                  return (
+                    <TableRow
+                      hover
+                      onClick={(event) =>
+                        handleClick(event, row.name.toString())
+                      }
+                      role="checkbox"
+                      aria-checked={isItemSelected}
+                      tabIndex={-1}
+                      key={row.name}
+                      selected={isItemSelected}
+                    >
+                      <TableCell padding="checkbox">
+                        <Checkbox
+                          color="primary"
+                          checked={isItemSelected}
+                          inputProps={{
+                            "aria-labelledby": labelId,
+                          }}
+                        />
+                      </TableCell>
+                      <TableCell
+                        component="th"
+                        id={labelId}
+                        scope="row"
+                        padding="none"
+                      >
+                        {row.email}
+                      </TableCell>
+                      <TableCell align="left">{row.name}</TableCell>
+                      <TableCell align="left">{row.tel}</TableCell>
+                      <TableCell align="left">{row.type}</TableCell>
+                    </TableRow>
+                  );
+                })}
+              {emptyRows > 0 && (
+                <TableRow>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+    </Box>
+  );
+}
--- src/pages/manage-page.tsx
+++ src/pages/manage-page.tsx
@@ -4,22 +4,12 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { useCallback, useEffect, useState } from "react";
-import Loading from "../components/loading";
+import { SyntheticEvent, useCallback, useEffect, useState } from "react";
 import Tab from "@mui/material/Tab";
 import Tabs from "@mui/material/Tabs";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
+import TableAccount from "../components/TableAccount";
 import AccountService from "../services/AccountService";
 import AccountModel from "../models/AccountModel";
-import { DeleteOutlined } from "@mui/icons-material";
-import StoreService from "../services/StoreService";
-import StoreModel from "../models/StoreModel";
 
 const theme = createTheme();
 
@@ -57,64 +47,72 @@ function a11yProps(index: number) {
 }
 
 export default function ManagePage() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [data, setDataUser] = useState<any>([]);
-  const [storeData, setStoreData] = useState<any>([]);
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = useState(0);
 
   const handleChange = useCallback(
-    (_event: React.SyntheticEvent, newValue: number) => {
+    (_event: SyntheticEvent, newValue: number) => {
       setValue(newValue);
     },
     []
   );
 
+  function createData(
+    id: string,
+    email: string,
+    name: string,
+    username: string,
+    status: boolean,
+    tel: string,
+    type: string,
+    verify: boolean,
+    password: string,
+    idStore: string
+  ): AccountModel | never {
+    return {
+      id,
+      email,
+      name,
+      username,
+      status,
+      tel,
+      type,
+      verify,
+      password,
+      idStore,
+    };
+  }
+
+  const [rows, setRows] = useState<AccountModel[]>([]);
+
   useEffect(() => {
-    console.log("manage-page.tsx");
-    setIsLoading(true);
-    AccountService.getUserAll().then((res: any) => {
-      var new_result = [];
-      for (var i = 0; i < res.length; i++) {
-        if (res[i].type !== "admin") {
-          const data = {
-            email: res[i].email.toString(),
-            id: res[i].id.toString(),
-            id_store: res[i].id_store.toString(),
-            name: res[i].name.toString(),
-            status: res[i].status,
-            tel: res[i].tel.toString(),
-            type: res[i].type.toString(),
-          };
-          new_result.push(data);
-        }
-      }
-      setDataUser(new_result);
-    });
-    StoreService.getStoreAll().then((res: any) => {
-      setStoreData(res);
-      setIsLoading(false);
+    AccountService.getUserAll().then((res) => {
+      console.log(res);
+
+      const data = res.map((item) =>
+        createData(
+          item.id,
+          item.email,
+          item.name,
+          item.username,
+          item.status,
+          item.tel,
+          item.type,
+          item.verify,
+          item.password,
+          item.idStore
+        )
+      );
+      setRows(data);
     });
   }, []);
 
-  const onDeleteAccount = (token: string | null) => {
-    setIsLoading(true);
-    AccountService.closeAccount(token).then((res: any) => {
-      setIsLoading(false);
-    });
-  };
-
-  const onDeleteStore = (token: string | null) => {
-    setIsLoading(true);
-    StoreService.closeStore(token).then((res: any) => {
-      setIsLoading(false);
-    });
-  };
-
-  if (isLoading) {
-    return <Loading />;
-  }
   return (
     <>
+      <style
+        dangerouslySetInnerHTML={{
+          __html: "\n    footer.footer {\n    display: none;\n}\n    ",
+        }}
+      />
       <ThemeProvider theme={theme}>
         <Container component="main">
           <CssBaseline />
@@ -145,60 +143,23 @@ export default function ManagePage() {
                 <Tab label="จัดการผู้ใช้งาน" {...a11yProps(0)} />
                 <Tab label="จัดการร้าน" {...a11yProps(1)} />
               </Tabs>
-              <TabPanel value={value} index={0}></TabPanel>
-              <TabPanel value={value} index={1}>
-                <TableContainer component={Paper}>
-                  <Table sx={{ minWidth: "100%" }} aria-label="simple table">
-                    <TableHead>
-                      <TableRow>
-                        <TableCell>ชื่อร้าน</TableCell>
-                        <TableCell align="right">ข้อมูลร้าน</TableCell>
-                        <TableCell align="right">เวลาทำการ</TableCell>
-                        <TableCell align="right">เบอร์โทรติดต่อ</TableCell>
-                        <TableCell align="center">เว็บไซต์</TableCell>
-                        <TableCell align="center">แผนที่</TableCell>
-                        <TableCell align="right">สถานะร้าน</TableCell>
-                        <TableCell align="right"></TableCell>
-                      </TableRow>
-                    </TableHead>
-                    <TableBody>
-                      {storeData.map((item: StoreModel) => (
-                        <TableRow
-                          key={item.name}
-                          sx={{
-                            "&:last-child td, &:last-child th": { border: 0 },
-                          }}
-                        >
-                          <TableCell component="th" scope="row">
-                            {item.name}
-                          </TableCell>
-                          <TableCell align="right">{item.address}</TableCell>
-                          <TableCell align="right">{item.open}</TableCell>
-                          <TableCell align="right">{item.tel}</TableCell>
-                          <TableCell align="right">{item.website}</TableCell>
-                          <TableCell align="right">{item.latitude}</TableCell>
-                          <TableCell align="right">
-                            {item.status === true ? "เปิด" : "ปิดทำการ"}
-                          </TableCell>
-                          <TableCell align="right">
-                            <DeleteOutlined
-                              sx={{ cursor: "pointer" }}
-                              onClick={() => {
-                                onDeleteStore(item.idstore);
-                              }}
-                            />
-                          </TableCell>
-                        </TableRow>
-                      ))}
-                    </TableBody>
-                  </Table>
-                </TableContainer>
+              <TabPanel value={value} index={0}>
+                <Box
+                  component="div"
+                  sx={{
+                    width: "100vh",
+                    textAlign: "center",
+                    marginLeft: 10,
+                  }}
+                >
+                  <TableAccount rows={rows} />
+                </Box>
               </TabPanel>
+              <TabPanel value={value} index={1}></TabPanel>
             </Box>
           </Box>
         </Container>
       </ThemeProvider>
-      );
     </>
   );
 }
